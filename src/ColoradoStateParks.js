37f27f895cd4d80385f2cc5f898d4042a20bbba5
wachira <wachiraa954@gmail.com>
2022-07-13 21:28:00 +0300

    ColoradoStatePark assgn done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde"; 
+function ColoradoStateParks(){
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  return(
+    <div>
+      <MesaVerde />
+   </div>
+  );
 
-  return <h1>Colorado State Parks!</h1>;
 }
+export default ColoradoStateParks;
